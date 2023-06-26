43f6d7bb9361e50ef130b251d4a9fed98768247b
Christian J Bennett <58433059+ChristianBDev@users.noreply.github.com>
2023-06-25 21:26:10 -0500

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,5 +1,6 @@
 fx_version 'cerulean'
 game 'rdr3'
+version '1.3.0'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
 ui_page "html/ui.html"
